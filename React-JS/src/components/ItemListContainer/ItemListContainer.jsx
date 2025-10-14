62b7e5cf8c57367600048a3174e78c8c150e0aab
Nicolas D. Rodriguez <ndr126@abc.gob.ar>
2025-10-14 13:47:49 -0300

    loading

--- React-JS/src/components/ItemListContainer/ItemListContainer.jsx
+++ React-JS/src/components/ItemListContainer/ItemListContainer.jsx
@@ -5,11 +5,13 @@ import { useEffect, useState } from 'react'
 export const ItemListContainer = ({ titulo }) => {
     //Estados
     const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
 
     //Llamadas a una api
     useEffect(() => {
         fetch('/data/products.json')
             .then((res) => {
+                setLoading(true)
                 if(!res.ok) 
                     throw new Error('Error al cargar los productos')
                 return res.json();
@@ -19,13 +21,20 @@ export const ItemListContainer = ({ titulo }) => {
             })
             .catch((error) => {
                 console.log(error);
+            })
+            .finally(() => {
+                setLoading(false); 
             });
     }, []);
 
     return (
         <div>
             <h1>{titulo}</h1>
-            <ItemList lista={products} />
+            {loading ? (
+                <p>Cargando...</p>
+            ) : (
+                <ItemList lista={products} />
+            )}
         </div>
     )
 }
